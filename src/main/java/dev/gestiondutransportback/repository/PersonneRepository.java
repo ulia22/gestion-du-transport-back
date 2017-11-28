994e146bf02a1349d2d20b953d7c906fd39b4887
Clément BARBASTE <barbaste.clement@gmail.com>
2017-11-28 14:35:43 +0100

    Fin modif back authentification

--- src/main/java/dev/gestiondutransportback/repository/PersonneRepository.java
+++ src/main/java/dev/gestiondutransportback/repository/PersonneRepository.java
@@ -2,8 +2,10 @@ package dev.gestiondutransportback.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import dev.gestiondutransportback.entity.Account;
 import dev.gestiondutransportback.entity.Personne;
 
 public interface PersonneRepository extends JpaRepository<Personne, Integer>{
 
+	public Personne findByAccount(Account a);
 }
---
+++ src/main/java/dev/gestiondutransportback/view/AccountEtPersonneView.java
@@ -0,0 +1,180 @@
+/**
+ * 
+ */
+package dev.gestiondutransportback.view;
+
+import dev.gestiondutransportback.entity.Account;
+import dev.gestiondutransportback.entity.Personne;
+import dev.gestiondutransportback.entity.Roles;
+
+/**
+ * @author ETY9
+ *
+ */
+public class AccountEtPersonneView {
+
+	private String email;
+
+	private Roles role;
+
+	private String nom;
+
+	private String prenom;
+
+	private String permis;
+
+	private String matricule;
+
+	/**
+	 * 
+	 */
+	public AccountEtPersonneView() {
+		super();
+	}
+
+	/**
+	 * 
+	 */
+	public AccountEtPersonneView(Account a, Personne p) {
+		this();
+		this.email = a.getEmail();
+		this.role = a.getRole();
+		this.nom = p.getNom();
+		this.prenom = p.getPrenom();
+		this.permis = p.getPermis();
+		this.matricule = p.getMatricule();
+	}
+
+	/**
+	 * @param email
+	 * @param mdp
+	 * @param role
+	 * @param nom
+	 * @param prenom
+	 * @param permis
+	 * @param matricule
+	 */
+	public AccountEtPersonneView(String email, Roles role, String nom, String prenom, String permis, String matricule) {
+		this();
+		this.email = email;
+		this.role = role;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.permis = permis;
+		this.matricule = matricule;
+	}
+
+	/**
+	 * Getter for email.
+	 * 
+	 * @return the email
+	 */
+	public String getEmail() {
+		return email;
+	}
+
+	/**
+	 * Setter for email
+	 * 
+	 * @param email
+	 *            the email to set
+	 */
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	/**
+	 * Getter for role.
+	 * 
+	 * @return the role
+	 */
+	public Roles getRole() {
+		return role;
+	}
+
+	/**
+	 * Setter for role
+	 * 
+	 * @param role
+	 *            the role to set
+	 */
+	public void setRole(Roles role) {
+		this.role = role;
+	}
+
+	/**
+	 * Getter for nom.
+	 * 
+	 * @return the nom
+	 */
+	public String getNom() {
+		return nom;
+	}
+
+	/**
+	 * Setter for nom
+	 * 
+	 * @param nom
+	 *            the nom to set
+	 */
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	/**
+	 * Getter for prenom.
+	 * 
+	 * @return the prenom
+	 */
+	public String getPrenom() {
+		return prenom;
+	}
+
+	/**
+	 * Setter for prenom
+	 * 
+	 * @param prenom
+	 *            the prenom to set
+	 */
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	/**
+	 * Getter for permis.
+	 * 
+	 * @return the permis
+	 */
+	public String getPermis() {
+		return permis;
+	}
+
+	/**
+	 * Setter for permis
+	 * 
+	 * @param permis
+	 *            the permis to set
+	 */
+	public void setPermis(String permis) {
+		this.permis = permis;
+	}
+
+	/**
+	 * Getter for matricule.
+	 * 
+	 * @return the matricule
+	 */
+	public String getMatricule() {
+		return matricule;
+	}
+
+	/**
+	 * Setter for matricule
+	 * 
+	 * @param matricule
+	 *            the matricule to set
+	 */
+	public void setMatricule(String matricule) {
+		this.matricule = matricule;
+	}
+}
