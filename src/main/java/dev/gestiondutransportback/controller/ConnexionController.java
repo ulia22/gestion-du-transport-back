0bd0bfc88378475109937eaad158c3e5a9f21028
Clément B <barbaste.clement@gmail.com>
2017-11-28 15:00:20 +0100

    Fin modif back authentification (#3)
    * Fin modif back authentification
    
    * Fin de review

--- src/main/java/dev/gestiondutransportback/controller/ConnexionController.java
+++ src/main/java/dev/gestiondutransportback/controller/ConnexionController.java
@@ -10,28 +10,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.gestiondutransportback.entity.Account;
+import dev.gestiondutransportback.entity.Personne;
 import dev.gestiondutransportback.exception.ConnexionException;
 import dev.gestiondutransportback.repository.AccountRepository;
+import dev.gestiondutransportback.repository.PersonneRepository;
+import dev.gestiondutransportback.view.AccountEtPersonneView;
 
 @RestController
 @RequestMapping("/connexion")
-@CrossOrigin(origins = {"*"})
+@CrossOrigin(origins = { "*" })
 public class ConnexionController {
-	
-	@Autowired private AccountRepository accountServ;
-	
+
+	@Autowired
+	private AccountRepository accountServ;
+	@Autowired
+	private PersonneRepository personneServ;
+
 	@PostMapping
-	public String postConnexion(@RequestBody Map<String, String> map) throws ConnexionException{
-		
+	public AccountEtPersonneView postConnexion(@RequestBody Map<String, String> map) throws ConnexionException {
+		System.out.println(map.get("email")+" "+map.get("password"));
 		Account account = accountServ.findByEmail(map.get("email"));
-		if (account.getMdp() == map.get("password")){
+		if (account != null && account.getMdp().equals(map.get("password"))) {
+			Personne personne = personneServ.findByAccount(account);
+			if (personne != null) {
+				return new AccountEtPersonneView(account, personne);
+			} else {
+				return new AccountEtPersonneView(account, new Personne());
+			}
 			
-			return account.getRole().toString();
 		} else {
 			throw new ConnexionException("L'email ou le mot de passe est incorecte");
 		}
-		
-		
+
 	}
 
 }
